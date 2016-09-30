3c806b1062474b2d9e352bb89e0b7128743071a9
Eli Galán <eliigl1992@gmail.com>
2016-09-30 18:30:58 +0200

    clase 30-09

--- ejercicio01.php
+++ ejercicio01.php
@@ -1,1 +1,25 @@
-TODO: 
+<!DOCTYPE html> 
+<html><head> 
+        <meta charset="UTF-8"> 
+        <title>ejemplo 01</title> 
+</head><body> 
+<?php 
+//inicializamos 
+echo "Array ordenado inicializado en la declaración y recorrido con un for: <hr>"; 
+$array1 = array ( 
+    'ID' => 6666, 
+    'Nombre' => 'Eli', 
+    'Apellido' => 'Galán Linares', 
+    'Edad' => 24 ,
+    'Nacionalidad' => 'Española' 
+); 
+
+
+//foreach clave=>elemento 
+echo "<br>"; 
+foreach ($array1 as $position=>$element){ 
+    echo $position . ": " . $element . '<br>'; 
+} 
+
+?> 
+</body></html>
